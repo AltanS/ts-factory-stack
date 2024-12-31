0ea52178a7d9eccadeaba643d46940d3e0fb752a
Altan Sarisin <altansarisin@gmail.com>
2024-12-31 20:57:17 +0100

    update schema

--- apps/frontend/drizzle/schema.ts
+++ apps/frontend/drizzle/schema.ts
@@ -28,3 +28,51 @@ export const users = pgTable('users', {
 
 export type InsertUser = InferInsertModel<typeof users>;
 export type SelectUser = InferSelectModel<typeof users>;
+
+
+export const articles = pgTable('articles', {
+  id: serial('id').primaryKey(),
+  title: text('title').notNull(),
+  slug: text('slug').notNull(),
+  content: text('content').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().$onUpdate(() => new Date()).notNull(),
+});
+
+
+export type InsertArticle = InferInsertModel<typeof articles>;
+export type SelectArticle = InferSelectModel<typeof articles>;
+
+export const articlesRelations = relations(articles, ({ many }) => ({
+  authors: many(users),
+  categories: many(categories),
+}));
+
+export const categories = pgTable('categories', {
+  id: serial('id').primaryKey(),
+  name: text('name').notNull(),
+}); 
+
+export type InsertCategory = InferInsertModel<typeof categories>;
+export type SelectCategory = InferSelectModel<typeof categories>;
+
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  articles: many(articles),
+}));  
+
+export const pages = pgTable('pages', {
+  id: serial('id').primaryKey(),
+  title: text('title').notNull(),
+  slug: text('slug').notNull(),
+  content: text('content').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().$onUpdate(() => new Date()).notNull(),
+});
+
+export type InsertPage = InferInsertModel<typeof pages>;
+export type SelectPage = InferSelectModel<typeof pages>;
+
+export const pagesRelations = relations(pages, ({ many, one }) => ({
+  authors: many(users),
+  category: one(categories),
+}));
